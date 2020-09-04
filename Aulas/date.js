ed29f71436718e0e75ef291c08f1b998199a0aa1
Thiago Gomes Araujo <thiagogomes543@hotmail.com>
2020-09-03 21:20:44 -0300

    Aulas Atualizdas por Pasta

--- Aulas/date.js
+++ Aulas/date.js
@@ -1,4 +1,4 @@
-//const data = new Date();
+const data = new Date();
 // Dia na semana( domingo = 0, segunda = 1, terca = 2
 // quarta = 3, quinta = 4, sexta = 5, sabado = 6)
 //console.log(data.getDay()); 
--- Aulas/hello-word.js
+++ Aulas/hello-word.js
@@ -1,1 +1,3 @@
-console.log("Hello Word");
+//console.log("Hello Word");
+let trocar = (a, b, c) => a.replace(b,c);
+console.log(trocar("Gosto muito de programar em VBA","VBA","JS"));

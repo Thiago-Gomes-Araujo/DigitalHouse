64dac1e9230f7c63da54594d84b872c1069f91ae
Thiago Gomes Araujo <thiagogomes543@hotmail.com>
2020-10-19 18:15:53 -0300

    atualizando o index.js

--- Express/MeuProjeto/routes/index.js
+++ Express/MeuProjeto/routes/index.js
@@ -1,9 +1,12 @@
 var express = require('express');
+const ContatoController = require('../controller/ContatoCrontroller');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.get("/contato", ContatoController.index);
 
 module.exports = router;

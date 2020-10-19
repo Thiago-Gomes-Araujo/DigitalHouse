e8a2f9ab1417fc563888dbd10cdc64739d26b6f8
Thiago-Gomes-Araujo <61298832+Thiago-Gomes-Araujo@users.noreply.github.com>
2020-10-19 09:18:49 -0400

    adc TesteController

---
+++ Express-2/MeuProjeto/controller/TesteController.js
@@ -0,0 +1,7 @@
+const TesteController = {
+    index:(req, res) => {
+        return res.render("Teste");
+    }
+};
+
+module.exports = TesteController;
--- Express-2/MeuProjeto/routes/index.js
+++ Express-2/MeuProjeto/routes/index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
 const ContatoController = require("../controller/ContatoController");
+const TesteController = require('../controller/TesteController');
+const ContatoController = require("../controller/TesteController");
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -8,5 +10,6 @@ router.get('/', function(req, res, next) {
 });
 
 router.get("/contato", ContatoController.index)
+router.get("/teste", TesteController.index)
 
 module.exports = router;

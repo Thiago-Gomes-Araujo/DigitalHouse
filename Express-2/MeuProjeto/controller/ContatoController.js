2f0821dc88c5a626d62223fc110e6ed93350f85e
Thiago-Gomes-Araujo <61298832+Thiago-Gomes-Araujo@users.noreply.github.com>
2020-10-19 10:02:30 -0400

    excluindo Express-2

--- Express-2/MeuProjeto/controller/ContatoController.js
+++
@@ -1,7 +0,0 @@
-const ContatoController = {
-    index:(req, res) => {
-        return res.render("Contato");
-    }
-};
-
-module.exports = ContatoController;
--- Express-2/MeuProjeto/controller/TesteController.js
+++
@@ -1,7 +0,0 @@
-const TesteController = {
-    index:(req, res) => {
-        return res.render("Teste");
-    }
-};
-
-module.exports = TesteController;

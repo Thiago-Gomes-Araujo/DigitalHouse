12816e169e7154b699b6600713d81367bc0a6ac3
Thiago Gomes Araujo <thiagogomes543@hotmail.com>
2020-10-21 17:56:38 -0300

    Update https.js

---
+++ https-2/https.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+const app = express();
+app.listen(32,() => console.log("Levantando um servidor com Express"));
+
+
+// Rotas 
+app.get('/home',function(req,res){
+    res.send('Listar Filmes')
+});

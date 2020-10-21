efc4bafd95578987f742af8d211de3a3d04e41c6
Thiago Gomes Araujo <thiagogomes543@hotmail.com>
2020-10-21 20:26:03 -0300

    Criando o celularController.js

---
+++ https-2/App/controllers/celularController.js
@@ -0,0 +1,37 @@
+const celulares = [
+    {
+        nome: 'Motorola Moto E6 Plus',
+        preco: 14999
+    },
+    {
+        nome: 'Motorola Moto G7',
+        preco: 19999
+    },
+    {
+        nome: 'Alcatel 5033A',
+        preco: 6999
+    },
+    {
+        nome: 'Samsung Galaxy A50',
+        preco: 33499
+    }
+];
+
+const celularContrller = {
+    
+    viewCelular: (req, res) =>{
+        /* res.send(celulares); */
+        /* res.send(req.query); */
+        const {max} = req.query;
+
+        for (i = 0; i < celulares.length; i++){
+            const a = celulares.filter((precos) =>{
+                return precos.preco < max
+            })
+            res.send(a)
+        }
+            
+    }
+
+}
+module.exports = celularContrller;
--- https-2/App/routes/index.js
+++ https-2/App/routes/index.js
@@ -1,11 +1,13 @@
 var express = require('express');
 var router = express.Router();
 const indexController = require('../controllers/indexController');
+const celularController = require('../controllers/celularController');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 router.get('/contato',indexController.viewContato);
+router.get('/celular/',celularController.viewCelular);
 
 module.exports = router;

5dce6cc3e5a98003ed99f5d414eb9475214545d2
Thiago Gomes Araujo <thiagogomes543@hotmail.com>
2020-10-20 22:09:39 -0300

    verificar o arquivo index.ejs

--- Aula17/MeuApp/routes/index.js
+++ Aula17/MeuApp/routes/index.js
@@ -3,7 +3,28 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+
+  let cards = [
+    {
+      'caminho_imagem': './images/card-vingadores.jpg',
+      'titulo': 'Primeiro Título',
+      'texto': 'Primeiro texto do card que temos para exibir',
+      'botao': 'Ver mais'
+    },
+    {
+      'caminho_imagem': './images/card-vingadores.jpg',
+      'titulo': 'Segundo Título',
+      'texto': 'Segundo texto do card que temos para exibir',
+      'botao': 'Ver mais'
+    },
+    {
+      'caminho_imagem': './images/card-vingadores.jpg',
+      'titulo': 'Terceiro Título',
+      'texto': 'Terceiro texto do card que temos para exibir',
+      'botao': 'Ver mais'
+    },
+  ];
+  res.render('index', {cards: cards});
 });
 
 module.exports = router;

8921e4990b0aa50ed427b8a2aab52d34aa519dc9
Thiago Gomes Araujo <thiagogomes543@hotmail.com>
2020-10-21 23:58:34 -0300

    Update protudoController.js

--- https-2/App/controllers/protudoController.js
+++ https-2/App/controllers/protudoController.js
@@ -1,12 +1,29 @@
+let produto = {
+    nome: null,
+    preco: null,
+}
+
 const produtoContrller = {
+
     viewForm: (req, res) =>{
         res.render('produto')
     },
 
-    saveForm: (req, res) =>{
+  /*   saveForm: (req, res) =>{
         console.log(req.body);
         const {nomeproduto, precoproduto} = req.body;
         res.send(nomeproduto)
+    } */
+    saveForm: (req, res) =>{
+
+
+        /* console.log(req.body);*/
+        const {nomeproduto, precoproduto} = req.body; 
+        let produto = {
+            nome: nomeproduto,
+            preco: precoproduto,
+        }
+        res.send(produto)
     }
 
 

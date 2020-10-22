8a223559ce211d183c45cdcd51fbf9b690b3cb35
Thiago-Gomes-Araujo <61298832+Thiago-Gomes-Araujo@users.noreply.github.com>
2020-10-22 19:18:33 -0400

    criando router contato

--- Aula18/MeuApp/routes/contato.js
+++ Aula18/MeuApp/routes/contato.js
@@ -2,8 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/contato', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/contato', function(req, res) {
+  res.send('Seu Contato animal');
 });
 
 module.exports = router;

55acd88fd72115e8876d790d6d5b88e26dec9000
Thiago-Gomes-Araujo <61298832+Thiago-Gomes-Araujo@users.noreply.github.com>
2020-10-29 07:23:05 -0400

    deletando arquivos

--- aula19-1/controllers/CardController.js
+++
@@ -1,7 +0,0 @@
-const { cards } = require("../data/data")
-
-module.exports = {
-  index(req, res, next) {
-    res.render('index', { cards });
-  }
-}
--- aula19-1/routes/index.js
+++
@@ -1,7 +0,0 @@
-var express = require('express');
-var router = express.Router();
-const CardController = require("../controllers/CardController");
-/* GET home page. */
-router.get('/', CardController.index);
-
-module.exports = router;

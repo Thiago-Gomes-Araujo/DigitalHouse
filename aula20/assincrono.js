fab0ac3006ed6b19e5fd63e38086264e9a59b23b
Thiago Gomes Araujo <thiagogomes543@hotmail.com>
2020-11-03 18:49:50 -0300

    atualizando

--- aula20/assincrono.js
+++ aula20/assincrono.js
@@ -1,8 +1,9 @@
-const axios = require('axios');
-const buscarUsuarioGitHub = async () =>{
-    let githubUsers = await axios.get('https://api.github.com/users/Thiago-Gomes')
-    /* return githubUsers; */
-    console.log(githubUsers)
+const axios = require("axios")
+
+const buscarUsuariosGitHub = async () => {
+  let githubUsers = await axios.get('https://api.github.com/users/vtorres96')
+
+  console.log(githubUsers)
 }
-/* console.log(buscarUsuarioGitHub()) */
-buscarUsuarioGitHub()
+
+buscarUsuariosGitHub()

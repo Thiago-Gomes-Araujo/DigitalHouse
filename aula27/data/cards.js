7ee11ea49a4486a5d3bf5361dee970f3ecde99c9
Thiago Gomes Araujo <thiagogomes543@hotmail.com>
2021-03-10 20:33:54 -0300

    teste

--- aula27/data/cards.js
+++ aula27/data/cards.js
@@ -1,18 +1,18 @@
 module.exports = [
   {
-    'path_image': '/images/card-vingadores.jpg',
+    'path_image': '/images/teste.jpg',
     'title': 'Primeiro Título',
     'text': 'Primeiro texto do card que temos para exibir',
     'button': 'Ver mais'
   },
   {
-    'path_image': '/images/card-vingadores.jpg',
+    'path_image': '/images/teste.jpg',
     'title': 'Segundo Título',
     'text': 'Segundo texto do card que temos para exibir',
     'button': 'Ver mais'
   },
   {
-    'path_image': '/images/card-vingadores.jpg',
+    'path_image': '/images/teste.jpg',
     'title': 'Terceiro Título',
     'text': 'Terceiro texto do card que temos para exibir',
     'button': 'Ver mais'
